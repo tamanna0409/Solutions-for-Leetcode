b7bd05f4d2ebcc5e85e4911b6534a0e3ab925c9c
tamanna0409 <gulhanetamanna@gmail.com>
2025-01-08 17:51:30 +0000

    solved

---
+++ 20. Valid Parentheses/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character> ();
+        for(char c: s.toCharArray()){
+            if(c=='(' || c == '{' || c== '['){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                else if(c==')'&& st.pop()!='('){
+                    //System.out.println(false);
+                    return false;
+                }
+                else if(c=='}'&& st.pop()!='{'){
+                    //System.out.println(false);
+                    return false;
+                }
+                else if(c==']'&& st.pop()!='['){
+                    //System.out.println(false);
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
