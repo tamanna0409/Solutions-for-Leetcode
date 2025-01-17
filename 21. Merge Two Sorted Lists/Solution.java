59b0d2414e3d12646efaedb637383c6702a21718
tamanna0409 <gulhanetamanna@gmail.com>
2025-01-17 18:23:37 +0000

    solved

---
+++ 21. Merge Two Sorted Lists/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+                ListNode c = new ListNode(0);
+                        ListNode x = c;
+
+                                while (list1 != null && list2 != null){
+                                            if(list1.val < list2.val){
+                                                            c.next = list1;
+                                                                            list1 = list1.next;
+                                                                                        }
+                                                                                                    else{
+                                                                                                                    c.next = list2;
+                                                                                                                                    list2 = list2.next;
+                                                                                                                                                }
+                                                                                                                                                            c = c.next;
+                                                                                                                                                                    }
+                                                                                                                                                                            if(list1 != null){
+                                                                                                                                                                                        c.next = list1;
+                                                                                                                                                                                                }
+                                                                                                                                                                                                        else{
+                                                                                                                                                                                                                    c.next = list2;
+                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                    return x.next;
+                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                        }
+}
