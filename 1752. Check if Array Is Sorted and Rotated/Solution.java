34169421da11bf95a78539581c8196e5a0ff57fa
tamanna0409 <gulhanetamanna@gmail.com>
2025-02-02 18:20:35 +0000

    solved

---
+++ 1752. Check if Array Is Sorted and Rotated/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean check(int[] nums) {
+     int c = 0, n = nums.length;
+     for (int i =1;i<n;i++){
+        if(nums[i-1]>nums[i]){
+            c++;
+        }
+    }
+    if(nums[0]<nums[n-1]){
+        c++;
+    }
+    return c<=1;
+}
+}
