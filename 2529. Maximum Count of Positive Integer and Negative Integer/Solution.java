2b964d8d2140ea0892ba7ee616f23b6d4e10b423
tamanna0409 <gulhanetamanna@gmail.com>
2025-03-12 16:38:09 +0000

    solved

---
+++ 2529. Maximum Count of Positive Integer and Negative Integer/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int pos=0;
+        int neg = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<0){
+                neg++;
+            }
+            else if(nums[i]>0){
+                pos++;
+            }
+        }
+        return Math.max(neg,pos);
+    }
+}
