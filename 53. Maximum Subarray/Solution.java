11d805005b08c156722624f0577ca238302a4966
tamanna0409 <gulhanetamanna@gmail.com>
2025-04-23 14:17:36 +0000

    solved

---
+++ 53. Maximum Subarray/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int cur =0;
+        for(int i=0;i<nums.length;i++){
+            
+
+            if(cur<0){
+                cur =0; 
+            }
+             cur += nums[i];
+
+            max = Math.max(cur,max);
+        }
+        return max;
+    }
+}
