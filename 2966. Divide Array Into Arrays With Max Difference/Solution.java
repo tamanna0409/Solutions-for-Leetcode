dabfcba1ae7e5f9d040c1ac40b8378045b0b6fbb
tamanna0409 <gulhanetamanna@gmail.com>
2025-06-18 18:31:31 +0000

    done

--- 2966. Divide Array Into Arrays With Max Difference/Solution.java
+++ 2966. Divide Array Into Arrays With Max Difference/Solution.java
@@ -1,15 +1,33 @@
 class Solution {
-    public List<List<Integer>> divideArray(int[] nums, int k) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
+    public int[][] divideArray(int[] a, int k) {
+        int m = Integer.MIN_VALUE;
 
-        for (int i = 0; i < nums.length; i += 3) {
-            if ( nums[i + 2] - nums[i] > k) {
-                return new ArrayList<>();
-            }
-            ans.add(Arrays.asList(nums[i], nums[i + 1], nums[i + 2]));
+        for (int i = 0; i < a.length; i++) {
+            m = Math.max(m, a[i]);
         }
 
-        return ans;
+        int[] c = new int[m + 1];
+
+        for (int i = 0; i < a.length; i++) {
+            c[a[i]]++;
+        }
+
+        int[][] r = new int[a.length / 3][3];
+        int i = 0, j = 0;
+
+        for (int x = 0; x <= m; x++) {
+            while (c[x] > 0) {
+                r[i][j++] = x;
+                if (j == 3) {
+                    if (Math.abs(r[i][0] - r[i][2]) > k) {
+                        return new int[0][0];
+                    }
+                    i++;
+                    j = 0;
+                }
+                c[x]--;
+            }
+        }
+        return r;
     }
 }
